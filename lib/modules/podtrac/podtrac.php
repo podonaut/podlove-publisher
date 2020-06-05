e0c6e52b7d6f375e66974bf0ec1b0f1a34d33146
Dave Keeshan <dkeeshan@vurple.com>
2020-06-05 16:23:28 -0700

    First working version of podtrac

--- lib/modules/podtrac/podtrac.php
+++ lib/modules/podtrac/podtrac.php
@@ -1,34 +1,24 @@
 <?php
-namespace Podlove\Modules\podtrac;
+namespace Podlove\Modules\Podtrac;
 use \Podlove\Model;
 
-class podtrac extends \Podlove\Modules\Base {
+class Podtrac extends \Podlove\Modules\Base {
 
-	protected $module_name = 'podtrac';
-	protected $module_description = 'Add Podtrac Analytics';
-	protected $module_group = 'External Services';
-	public function load() {
-			add_action( 'init', array( $this, 'register_hooks' ) );
-// 			$this->register_option( 'fyyd_verifycode', 'string', array(
-// 					'label'       => __( 'fyyd verifycode', 'podlove-podcasting-plugin-for-wordpress' ),
-// 					'description' => __( 'Code to verify your ownership at fyyd', 'podlove-podcasting-plugin-for-wordpress' ),
-// 					'html'        => array(
-// 							'class' => 'regular-text podlove-check-input',
-// 							'data-podlove-input-type' => 'text',
-// 							'placeholder' => 'yourverifycodehere'
-// 					)
-// 			) );
-	}
+    protected $module_name = 'Podtrac';
+    protected $module_description = 'Add Podtrac Analytics';
+    protected $module_group = 'external services';
+    public function load() {
+        //add_action( 'init', array( $this, 'register_hooks' ) );
+        add_filter('podlove_enclosure_url', function ($original_url) {
+            $ext            = pathinfo($original_url, PATHINFO_EXTENSION);
+            $schemeless_url = preg_replace('/^https?:\/\//', '', $original_url);
+            $podtrac_prefix = "https://dts.podtrac.com/redirect.$ext/";
+            return $podtrac_prefix . $schemeless_url;
+        });
+    }
 
 
-	public function register_hooks() {
-// 			$fyyd_verifycode = $this->get_module_option( 'fyyd_verifycode' );
-// 			if ( ! $fyyd_verifycode )
-// 					return;
-// 			add_action( 'podlove_rss2_head', function( $feed ) use ( $fyyd_verifycode ) {
-// 					echo "\n\t" . sprintf( '<fyyd:verify xmlns:fyyd="https://fyyd.de/fyyd-ns/">%s</fyyd:verify>'."\n\t", $fyyd_verifycode );
-// 			} );
-
-	}
+    public function register_hooks() {
+    }
 
 }
