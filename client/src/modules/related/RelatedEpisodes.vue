podonaut/podlove-publisher

commit 0a9df35dcc11db2637aad470747ff447ca978b93

chore: bump redux 4 -> 5

client/src/modules/related/RelatedEpisodes.vue
@@ -25,7 +25,6 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { mapState, injectStore } from 'redux-vuex'
 
 import { selectors } from '@store'
 
@@ -33,6 +32,10 @@ import Module from '@components/module/Module.vue'
 import PodloveListbox, { OptionObject } from '@components/combobox/Combobox.vue'
 import Tag from '@components/tag/Tag.vue';
 import * as related from '@store/relatedEpisodes.store'
+import { injectAppDispatch, mapAppState } from '@store/vue'
+import { PodloveEpisodeList } from '../../types/relatedEpisodes.types'
+
+type RelatedOption = OptionObject
 
 export default defineComponent({
   components: {
@@ -42,31 +45,37 @@ export default defineComponent({
   },
   setup() {
     return {
-      state: mapState({
+      state: mapAppState({
         episodeList: selectors.relatedEpisodes.episodeList,
         selectEpisodes: selectors.relatedEpisodes.selectEpisode,
       }),
-      dispatch: injectStore().dispatch,
+      dispatch: injectAppDispatch(),
     }
   },
   created() {
     this.dispatch(related.init())
   },
   computed: {
+    episodeOptions(): RelatedOption[] {
+      return this.state.episodeList.map((episode: PodloveEpisodeList) => ({
+        id: episode.episode_id,
+        title: episode.episode_title,
+      }))
+    },
     fullEpisodeList() : Array<OptionObject> {
-      if (this.state.episodeList.length == 0)
-        return this.state.episodeList
+      if (this.episodeOptions.length == 0)
+        return this.episodeOptions
       if (this.state.selectEpisodes.length == 0) {
         const selectAllEpisodes = { id: 0, title: "Select all episodes"}
-        return [selectAllEpisodes, ...this.state.episodeList]
+        return [selectAllEpisodes, ...this.episodeOptions]
       }
       else {
         const selectAllEpisodes = { id: -1, title: "Deselect all episodes"}
-        return [selectAllEpisodes, ...this.state.episodeList]
+        return [selectAllEpisodes, ...this.episodeOptions]
       }
     },
     selectEpisodeNames() : Array<OptionObject> | null {
-      return this.state.episodeList?.filter( (episode: OptionObject) => {
+      return this.episodeOptions.filter((episode: RelatedOption) => {
         return this.state.selectEpisodes.includes( episode.id )
       })
     }

client/src/modules/auphonic/Auphonic.vue
@@ -13,13 +13,12 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { selectors } from '@store'
-
-import { injectStore, mapState } from 'redux-vuex'
 import * as auphonic from '@store/auphonic.store'
 import ManageProductionForm from './components/ManageProductionForm.vue'
 import StartScreen from './components/StartScreen.vue'
 import AuphonicLogo from './components/Logo.vue'
 import Module from '@components/module/Module.vue'
+import { injectAppDispatch, mapAppState } from '@store/vue'
 
 export default defineComponent({
   components: {
@@ -31,11 +30,11 @@ export default defineComponent({
 
   setup() {
     return {
-      state: mapState({
+      state: mapAppState({
         productionId: selectors.auphonic.productionId,
         isInitializing: selectors.auphonic.isInitializing,
       }),
-      dispatch: injectStore().dispatch,
+      dispatch: injectAppDispatch(),
     }
   },
 

client/src/modules/auphonic/components/FileChooser.vue
@@ -105,11 +105,11 @@
 import { defineComponent } from 'vue'
 import { selectors } from '@store'
 
-import { injectStore, mapState } from 'redux-vuex'
 import * as auphonic from '@store/auphonic.store'
-import { Service } from '@store/auphonic.store'
+import { FileSelection, Service } from '@store/auphonic.store'
 import { update as updateEpisode } from '@store/episode.store'
 import { disableSlugAutogen } from '@store/mediafiles.store'
+import { injectAppDispatch, mapAppState } from '@store/vue'
 
 import {
   Listbox,
@@ -122,6 +122,15 @@ import { CheckIcon, ChevronUpDownIcon as SelectorIcon } from '@heroicons/vue/24/
 import { get } from 'lodash'
 
 import { DocumentCheckIcon } from '@heroicons/vue/24/outline'
+
+type LocalFileSelection = {
+  urlValue?: string | null
+  fileValue?: File | string | null
+  currentServiceSelection?: string | null
+  fileSelection?: string | null
+}
+
+type ServiceFilesMap = Record<string, string[] | null>
 
 export default defineComponent({
   props: ['file_key', 'track_index'],
@@ -138,7 +147,7 @@ export default defineComponent({
   },
 
   setup() {
-    const state = mapState({
+    const state = mapAppState({
       services: selectors.auphonic.incomingServices,
       serviceFiles: selectors.auphonic.serviceFiles,
       fileSelections: selectors.auphonic.fileSelections,
@@ -147,7 +156,7 @@ export default defineComponent({
 
     return {
       state,
-      dispatch: injectStore().dispatch,
+      dispatch: injectAppDispatch(),
     }
   },
 
@@ -198,26 +207,32 @@ export default defineComponent({
     services(): Service[] {
       return this.state.services
     },
-    currentService(): Service {
-      return this.state.services.find((s: Service) => s.uuid === this.currentServiceSelection)
+    currentService(): Service | null {
+      return this.state.services.find((s: Service) => s.uuid === this.currentServiceSelection) || null
+    },
+    serviceFilesMap(): ServiceFilesMap {
+      return this.state.serviceFiles as ServiceFilesMap
     },
     serviceFiles(): string[] | null {
-      return get(this.state, ['serviceFiles', this.currentServiceSelection], null)
+      return this.currentServiceSelection
+        ? this.serviceFilesMap[this.currentServiceSelection] ?? null
+        : null
     },
-    fileSelection(): any {
-      return this.state.fileSelections[this.file_key]
+    fileSelection(): LocalFileSelection {
+      return get(this.state.fileSelections, [this.file_key], {}) as LocalFileSelection
     },
-    filenameSelectedForUpload(): any {
-      return this.fileSelection?.fileValue?.name
+    filenameSelectedForUpload(): string | null {
+      const fileValue = this.fileSelection.fileValue
+      return fileValue instanceof File ? fileValue.name : null
     },
-    currentServiceSelection(): any {
-      return this.fileSelection?.currentServiceSelection
+    currentServiceSelection(): string | null {
+      return this.fileSelection.currentServiceSelection || null
     },
     urlFieldValue(): string {
-      return this.fileSelection.urlValue
+      return this.fileSelection.urlValue || ''
     },
     externalFileFieldValue(): string {
-      return this.fileSelection.fileSelection
+      return this.fileSelection.fileSelection || ''
     },
     slugCandidate(): string | null {
       if (this.currentServiceSelection == 'url') {

client/src/modules/auphonic/components/ManageProductionForm.vue
@@ -350,8 +350,7 @@ import PodloveButton from '@components/button/Button.vue'
 import FileChooser from './FileChooser.vue'
 
 import { State, selectors } from '@store'
-
-import { injectStore, mapState } from 'redux-vuex'
+import { injectAppDispatch, mapAppState } from '@store/vue'
 import * as auphonic from '@store/auphonic.store'
 import { Production, AudioTrack } from '@store/auphonic.store'
 
@@ -404,13 +403,13 @@ export default defineComponent({
 
   setup() {
     return {
-      state: mapState({
+      state: mapAppState({
         production: selectors.auphonic.production,
         tracks: selectors.auphonic.tracks,
         isSaving: selectors.auphonic.isSaving,
         progress: (state: State) => (key: string) => selectors.progress.progress(state, key),
       }),
-      dispatch: injectStore().dispatch,
+      dispatch: injectAppDispatch(),
     }
   },
 
@@ -463,13 +462,13 @@ export default defineComponent({
       this.updateTrack('identifier_new', (event.target as HTMLInputElement).value, index)
     },
     uploadProgress(key: string): string | null {
-      return this.state.progress(key) || null
+      return this.state.progress(key)?.toString() || null
     },
   },
 
   computed: {
     production(): Production {
-      return this.state.production || {}
+      return this.state.production!
     },
     isSaving(): boolean {
       return this.state.isSaving
@@ -487,7 +486,7 @@ export default defineComponent({
       return this.state.tracks || []
     },
     isMultitrack(): boolean {
-      return this.state.production && this.state.production.is_multitrack
+      return !!this.state.production?.is_multitrack
     },
   },
 })

client/src/modules/auphonic/components/SelectPreset.vue
@@ -59,10 +59,8 @@
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
 import { selectors } from '@store'
-
-import { injectStore, mapState } from 'redux-vuex'
-
 import * as auphonic from '@store/auphonic.store'
+import { injectAppDispatch, mapAppState } from '@store/vue'
 
 import {
   Listbox,
@@ -95,11 +93,11 @@ export default defineComponent({
 
   setup() {
     return {
-      state: mapState({
+      state: mapAppState({
         presets: selectors.auphonic.presets,
         currentPreset: selectors.auphonic.preset,
       }),
-      dispatch: injectStore().dispatch,
+      dispatch: injectAppDispatch(),
     }
   },
 
@@ -111,7 +109,7 @@ export default defineComponent({
 
   computed: {
     presets(): PresetWithSelectionData[] {
-      return this.state.presets
+      return (this.state.presets || [])
         .map((preset: Preset) => {
           const date = preset.creation_time.split('T')[0]
           const name = preset.preset_name
@@ -119,7 +117,7 @@ export default defineComponent({
 
           return { ...preset, _select: { name, date, is_multitrack } }
         })
-        .toSorted((a: any, b: any) => {
+        .toSorted((a: PresetWithSelectionData, b: PresetWithSelectionData) => {
           const nameA = a._select.name.toUpperCase()
           const nameB = b._select.name.toUpperCase()
 
@@ -130,7 +128,7 @@ export default defineComponent({
     },
     currentPreset(): PresetWithSelectionData | null | undefined {
       const currentPreset = this.state.currentPreset
-      return this.presets.find((p) => p.uuid === currentPreset)
+      return this.presets.find((p) => p.uuid === currentPreset?.uuid)
     },
   },
 })

client/src/modules/auphonic/components/SelectProduction.vue
@@ -66,11 +66,9 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { selectors } from '@store'
-
-import { injectStore, mapState } from 'redux-vuex'
-
 import * as auphonic from '@store/auphonic.store'
 import { Production } from '@store/auphonic.store'
+import { injectAppDispatch, mapAppState } from '@store/vue'
 
 import {
   Listbox,
@@ -101,11 +99,11 @@ export default defineComponent({
 
   setup() {
     return {
-      state: mapState({
+      state: mapAppState({
         productions: selectors.auphonic.productions,
         production: selectors.auphonic.production,
       }),
-      dispatch: injectStore().dispatch,
+      dispatch: injectAppDispatch(),
     }
   },
 
@@ -117,7 +115,7 @@ export default defineComponent({
 
   computed: {
     productions(): ProductionWithSelectionData[] {
-      return this.state.productions.map((production: Production) => {
+      return (this.state.productions || []).map((production: Production) => {
         const date = production.creation_time.split('T')[0]
         const name = production.metadata.title
 

client/src/modules/auphonic/components/StartScreen.vue
@@ -84,8 +84,7 @@ import Module from '@components/module/Module.vue'
 import PodloveButton from '@components/button/Button.vue'
 import { PlusIcon as PlusSmIcon, ArrowPathIcon as RefreshIcon } from '@heroicons/vue/24/outline'
 import { selectors } from '@store'
-
-import { injectStore, mapState } from 'redux-vuex'
+import { injectAppDispatch, mapAppState } from '@store/vue'
 import * as auphonic from '@store/auphonic.store'
 import SelectProduction from '../components/SelectProduction.vue'
 import SelectPreset from '../components/SelectPreset.vue'
@@ -106,12 +105,12 @@ export default defineComponent({
   },
   setup() {
     return {
-      state: mapState({
-        presetUUID: selectors.auphonic.preset,
+      state: mapAppState({
+        preset: selectors.auphonic.preset,
         presets: selectors.auphonic.presets,
         isInitializing: selectors.auphonic.isInitializing,
       }),
-      dispatch: injectStore().dispatch,
+      dispatch: injectAppDispatch(),
     }
   },
 
@@ -141,8 +140,8 @@ export default defineComponent({
   },
 
   computed: {
-    preset(): auphonic.Preset {
-      return this.state.presets.find((p: auphonic.Preset) => p.uuid === this.state.presetUUID)
+    preset(): auphonic.Preset | null {
+      return this.state.preset
     },
     isInitializing(): boolean {
       return this.state.isInitializing

client/src/modules/auphonic/components/WebhookToggle.vue
@@ -29,9 +29,9 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { Switch, SwitchGroup, SwitchLabel } from '@headlessui/vue'
-import { injectStore, mapState } from 'redux-vuex'
 import { selectors } from '@store'
 import * as auphonic from '@store/auphonic.store'
+import { injectAppDispatch, mapAppState } from '@store/vue'
 
 export default defineComponent({
   components: {
@@ -52,10 +52,10 @@ export default defineComponent({
   },
   setup() {
     return {
-      state: mapState({
+      state: mapAppState({
         publishWhenDone: selectors.auphonic.publishWhenDone,
       }),
-      dispatch: injectStore().dispatch,
+      dispatch: injectAppDispatch(),
     }
   },
   // fixme: set state initially when episode data is available; somewhere in saga/store?

commit b86b3d7213669b7a59c91d3234740b19877d2bf0

fix: related episodes
regression from 0a9df35d

client/src/modules/related/RelatedEpisodes.vue
@@ -84,9 +84,7 @@ export default defineComponent({
     updateRelEpisodes(newSelectedItems: Array<Number>) {
       if (newSelectedItems.includes(0)) {
         // Select all
-        this.state.selectEpisodes = this.state.episodeList.map(function(item:any) {
-          return item.id
-        })
+        this.state.selectEpisodes = this.episodeOptions.map((item: RelatedOption) => item.id)
       }
       else if (newSelectedItems.includes(-1)) {
         // deselect all
@@ -101,7 +99,12 @@ export default defineComponent({
       const idx = this.state.selectEpisodes.findIndex( (elem: Number) => {
         return elem == removeId;
       })
+      if (idx === -1) {
+        return
+      }
+
       this.state.selectEpisodes.splice(idx, 1);
+      this.dispatch(related.setSelectedEpisodes([...this.state.selectEpisodes]))
     }
   }
 })

client/src/sagas/relatedEpisodes.sagas.ts
@@ -8,6 +8,11 @@ import { PodloveEpisodeList } from '../types/relatedEpisodes.types'
 import { takeFirst } from './helper'
 import * as relatedEpisodesStore from '@store/relatedEpisodes.store'
 
+type EpisodeApiListItem = {
+  id: number
+  title: string
+}
+
 function* relatedEpisodesSaga(): any {
   const apiClient: PodloveApiClient = yield createApi()
 
@@ -20,14 +25,17 @@ function* initialize(api: PodloveApiClient) {
 
   const [relatedEpisodes, episodeList ]: [
     { result: Number[] },
-    { result: PodloveEpisodeList[] }
+    { result: EpisodeApiListItem[] }
   ] = yield Promise.all([
     api.get(`episodes/${episodeId}/related?status=all`),
     api.get('episodes?status=all&sort_by=post_id&order_by=asc')
   ])
 
   const related = get(relatedEpisodes, ['result', 'relatedEpisodes'], [])
-  const episodes = get(episodeList, ['result', 'results'], [])
+  const episodes = get(episodeList, ['result', 'results'], []).map((episode: EpisodeApiListItem) => ({
+    episode_id: episode.id,
+    episode_title: episode.title,
+  }))
 
   const arr = related.map( (r : any) => (r.related_episode_id))
 