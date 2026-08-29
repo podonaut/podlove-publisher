b80dcc47f6a9e664cb3fa64dda0d6d5df4f8cc12
Eric Teubert <eric@ericteubert.de>
2026-08-29 13:33:26 +0800

    release 4.5.6

--- podlove.php
+++ podlove.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Podlove Podcast Publisher
  * Plugin URI:  https://podlove.org/podlove-podcast-publisher/
- * Version: 4.5.5
+ * Version: 4.5.6
  * Requires at least: 5.7.0
  * Requires PHP: 8.0
  * Author:      Podlove
